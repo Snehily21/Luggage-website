254a1f2a58bd9fd5a5022bec982bf22e608c5933
Snehily21 <106708156+Snehily21@users.noreply.github.com>
2022-06-01 22:34:42 +0100

    Add files via upload

---
+++ bootstrap landing page/index.js
@@ -0,0 +1,31 @@
+let botton1 = document.getElementById('btn1');
+
+
+botton1.addEventListener('click',() => {botton1.innerHTML='Not Hosted Yet';
+
+});
+let botton2 = document.getElementById('btn2');
+
+
+botton2.addEventListener('click',() => {botton2.innerHTML='Not Hosted Yet';
+
+});
+let botton3 = document.getElementById('btn3');
+
+
+botton3.addEventListener('click',() => {botton3.innerHTML='Not Hosted Yet';
+
+});
+
+let login = document.querySelector('.login');
+
+
+login.addEventListener('click',() => {login.innerHTML='Not Hosted Yet';
+
+});
+let sign_up  = document.querySelector('.Sign-up');
+
+
+sign_up.addEventListener('click',() => {sign_up.innerHTML='Not Hosted Yet';
+
+});
